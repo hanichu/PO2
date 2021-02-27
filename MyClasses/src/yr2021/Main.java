d7e14bc7a558ca6b791c4ccd4a0bd3cdc35ad9d7
Hendry <hendrymino@gmail.com>
2021-02-27 19:54:11 +0100

    up

--- MyClasses/src/yr2021/Main.java
+++ MyClasses/src/yr2021/Main.java
@@ -1,7 +1,39 @@
 package yr2021;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ciao Tanjin");
+
+    public static class Animal {
+        protected int weight;
+
+        public void eat(Animal a) {
+            this.weight += a.weight;
+        }
     }
+
+    public static class Dog extends Animal {
+        public void bark() { }
+
+        public void marry(Dog d) {}
+
+        @Override
+        public void eat(Animal a) {
+            this.weight += a.weight / 2;
+        }
+    }
+
+    private static Animal makeAnimal() {
+        return new Dog();
+    }
+
+    public static void main(String[] args) {
+
+        Animal fido = makeAnimal();
+        Dog lilly = new Dog();
+        fido.eat(lilly);        // SUBSUMPTION
+
+        lilly.marry(fido);      // non compila perché fido : Animal
+
+    }
+
 }
+
