119c91a33b7915eef1934db7ae69860fcc234cd7
hanichu <hendrymino@gmail.com>
2021-03-10 16:44:25 +0100

    Revert "Lezione 24/2/21"
    This reverts commit e3814c46b30ef492aef64e0f68f52b67ea464afb.

--- 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/tinyjdk/Iterable.java
+++
@@ -1,5 +0,0 @@
-package it.unive.dais.po2.aa2020_21.tinyjdk;
-
-public interface Iterable<T> {
-    Iterator<T> iterator();
-}
--- 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/tinyjdk/Iterator.java
+++
@@ -1,6 +0,0 @@
-package it.unive.dais.po2.aa2020_21.tinyjdk;
-
-public interface Iterator<T> {
-    boolean hasNext();
-    T next();
-}
