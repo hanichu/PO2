5ca22914b1e2751b0912acc1d3a06b48c382a04a
Alvise Spanò <spano.alvise@gmail.com>
2021-03-24 15:40:18 +0100

    Lezione 24/3/21

--- 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/tinyjdk/Map.java
+++ 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/tinyjdk/Map.java
@@ -5,9 +5,16 @@ import org.jetbrains.annotations.Nullable;
 
 public interface Map<K, V> extends Iterable<Pair<K, V>> {
     void put(@NotNull K key, @Nullable V value);
-    @Nullable V get(@NotNull K key) throws PairMap.NotFoundException;
+    @Nullable V get(@NotNull K key) throws NotFoundException;
     int size();
     void remove(@NotNull K key);
+
+    class NotFoundException extends Exception {
+        public NotFoundException(Object k) {
+            super(String.format("key = %s", k));
+        }
+    }
+
 }
 
 
