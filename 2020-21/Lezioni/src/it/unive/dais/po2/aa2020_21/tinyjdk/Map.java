01eeb5ab114eea17f06f56f0f7ab35b60963fde9
hanichu <hendrymino@gmail.com>
2021-04-04 20:04:09 +0200

    Revert "Lezione 22/3/21"
    This reverts commit e004729ddb79ee000bc4ae3d9e61d71fd3ba5d79.

--- 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/tinyjdk/Map.java
+++ 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/tinyjdk/Map.java
@@ -5,9 +5,15 @@ import org.jetbrains.annotations.Nullable;
 
 public interface Map<K, V> extends Iterable<Pair<K, V>> {
     void put(@NotNull K key, @Nullable V value);
-    @Nullable V get(@NotNull K key) throws PairMap.NotFoundException;
+    @Nullable V get(@NotNull K key) throws KeyNotFoundException;
     int size();
     void remove(@NotNull K key);
+
+    class KeyNotFoundException extends Exception {
+        public KeyNotFoundException(Object k) {
+            super(String.format("key = %s", k));
+        }
+    }
 }
 
 
--- 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/tinyjdk/PairMap.java
+++ 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/tinyjdk/PairMap.java
@@ -4,13 +4,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class PairMap<K, V> implements Map<K, V> {
-    @NotNull
     private final ArrayList<Pair<K, V>> a = new ArrayList<>();
 
     @Nullable
-    private Pair<Integer, Pair<K, V>> search(@NotNull K key) {
+    private Pair<Integer, Pair<K, V>> search(K key) {
         for (int i = 0; i < a.size(); ++i) {
-            @NotNull final Pair<K, V> p = a.get(i);
+            Pair<K, V> p = a.get(i);
             K e = p.first;
             if (e.equals(key)) {
                 return new Pair<>(i, p);
@@ -20,7 +19,7 @@ public class PairMap<K, V> implements Map<K, V> {
     }
 
     @Override
-    public void put(@NotNull K key, @Nullable V value) {
+    public void put(K key, V value) {
         Pair<Integer, Pair<K, V>> r = search(key);
         if (r != null) {
             a.set(r.first, new Pair<>(r.second.first, value));
@@ -30,7 +29,8 @@ public class PairMap<K, V> implements Map<K, V> {
     }
 
     @Override
-    public V get(@NotNull K key) throws NotFoundException {
+    @NotNull
+    public V get(K key) throws NotFoundException {
         Iterator<Pair<K, V>> it = iterator();   // TODO usare search()
         while (it.hasNext()) {
             Pair<K, V> p = it.next();
@@ -45,7 +45,7 @@ public class PairMap<K, V> implements Map<K, V> {
     }
 
     @Override
-    public void remove(@NotNull K key) {
+    public void remove(K key) {
         Pair<Integer, Pair<K, V>> r = search(key);
         if (r != null) {
             a.remove(r.first);
@@ -54,14 +54,12 @@ public class PairMap<K, V> implements Map<K, V> {
 
     @Override
     public Iterator<Pair<K, V>> iterator() {
-        return a.iterator();
+        return null; // TODO
     }
 
-    @Nullable
     public static class NotFoundException extends Exception {
         public NotFoundException(Object k) {
             super(String.format("key = %s", k));
         }
     }
-
 }
